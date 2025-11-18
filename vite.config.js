d74a23f9aff8f501584bd19575b9286f4ca6e1d8
capmahin <mahinulhuque1996@gmail.com>
2025-11-18 12:56:09 +0600

    add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-export default {
-    root: 'src/',
-    publicDir: '../static/',
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    root: 'src',
+    publicDir: '../static',
     base: './',
     build: {
         outDir: 'dist',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name].[ext]',
+                chunkFileNames: 'assets/[name].js',
+                entryFileNames: 'assets/[name].js'
+            }
+        }
     }
-}
+})
